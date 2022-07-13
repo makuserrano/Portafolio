77f81f3592cb38705cc89ee4f071de700cd49e83
Maku <maku_serrano@outlook.com>
2022-07-13 16:22:17 -0300

    act 13/7

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
+//btn responsive
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 
-function toggleMenu(){
+function toggleMenu() {
     menu.classList.toggle("menu_opened");
 }
 
@@ -13,7 +13,37 @@ closeMenuBtn.addEventListener("click", toggleMenu);
 
 const menuLinks = document.querySelectorAll(".menu a[href^='#']");
 menuLinks.forEach(menuLink => {
-    menuLink.addEventListener("click", function(){
+    menuLink.addEventListener("click", function () {
         menu.classList.remove("menu_opened");
     })
-    })
+})
+
+// Enviar mail 
+
+function validate() {
+    let name = document.querySelector('.name')
+    let email = document.querySelector('.email')
+    let msg = document.querySelector('.message')
+    let sendBtn = document.querySelector('.send-btn')
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg == "") {
+            emptyerror();
+        } else {
+            sendmail(name.value, email.value, msg.value);
+            success();
+        }
+    });
+
+}
+
+validate();
+
+function sendmail(name, email, msg) {
+    emailjs.send("service_1tvr1eq", "template_duico0x", {
+        to_name: email,
+        from_name: name,
+        message: msg,
+    });
+}
